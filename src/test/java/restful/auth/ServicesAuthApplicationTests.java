d6a1c74bf6f3170ab5534bb2db253aeaf06c223d
Ednilo Pinheiro <ednilo.pinheiro@gmail.com>
2021-09-19 10:10:08 -0300

    First commit

---
+++ src/test/java/restful/auth/ServicesAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package restful.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
